0eff9630ac3e08f6c55add285db02dd1ced6a7aa
Piotr Wendt <wendtpiotr333@gmail.com>
2024-05-12 21:40:55 +0200

    feat: create fav icon, change description and title

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,50 +9,12 @@ import Head from "next/head";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <link
-          type="image/png"
-          sizes="16x16"
-          rel="icon"
-          href="./icons8-lab-16.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="72x72"
-          href="./icons8-lab-72.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="96x96"
-          href="./icons8-lab-96.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          type="image/png"
-          sizes="57x57"
-          href="./icons8-lab-57.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          type="image/png"
-          sizes="60x60"
-          href="./icons8-lab-60.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          type="image/png"
-          sizes="72x72"
-          href="./icons8-lab-72.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          type="image/png"
-          sizes="76x76"
-          href="./icons8-lab-76.png"
-        />
-      </Head>
+      <link
+        rel="icon"
+        href="/icon?<generated>"
+        type="image/<generated>"
+        sizes="<generated>"
+      />
       <body>{children}</body>
     </html>
   );
